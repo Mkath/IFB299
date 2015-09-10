7870f58625f49de472cf54241747491e7be046c8
SeanL94 <sean.little@connect.qut.edu.au>
2015-09-10 10:48:49 +1000

    Changes to javascript file 10/09

--- main_javascript.js
+++ main_javascript.js
@@ -10,6 +10,25 @@ function validate(form){
 	}
   return validation;
 }
+
+function signinvalidate(form){
+	//validation variable is set to stop the form if venuename is null
+	var validation = true;
+	//if the field is null, the error message is made visible, field is focussed on
+	//and variable - 'validation', returns false.
+	if (form.username.value == ""){
+		document.getElementById("usernameMissing").style.visibility = "visible";
+		form.username.focus();
+		validation = false;
+	}
+	if (form.password.value == ""){
+		document.getElementById("passwordMissing").style.visibility = "visible";
+		form.password.focus();
+		validation = false;
+	}
+  return validation;
+}
+
 //this function is used to make the suburb element invisible again
 function invisible(element){
 	document.getElementById(element).style.visibility = "hidden";
